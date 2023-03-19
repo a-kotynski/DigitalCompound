e0cd75fa61a65dfaa781e7e59c418055a4441259
A <a.kotynski@hotmail.com>
2023-03-19 09:17:57 +0100

    Dropped the existing database, removed initial migration, added new migration.

--- DigitalCompoundInfrastructure/Data/Migrations/20230319074934_InitialCreate.Designer.cs
+++ DigitalCompoundInfrastructure/Data/Migrations/20230319074934_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using DigitalCompoundInfrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DigitalCompoundInfrastructure.Data.Migrations
 {
     [DbContext(typeof(DigitalCompoundDbContext))]
-    partial class DigitalCompoundDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230319074934_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
