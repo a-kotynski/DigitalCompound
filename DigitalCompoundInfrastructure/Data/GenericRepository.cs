3deaea15e1e7e022d6ae8b7e55cf7ade5abbe405
A <a.kotynski@hotmail.com>
2023-04-05 10:46:41 +0200

    Added implementation of IGenericRepository interface

--- DigitalCompoundInfrastructure/Data/GenericRepository.cs
+++ DigitalCompoundInfrastructure/Data/GenericRepository.cs
@@ -1,6 +1,9 @@
+using System.Runtime.CompilerServices;
 using DigitalCompoundCore.Entities;
 using DigitalCompoundCore.Intefaces;
+using DigitalCompoundCore.Specifications;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DigitalCompoundInfrastructure.Data;
 
@@ -14,11 +17,27 @@ public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
 
     public async Task<T> GetByIdAsync(int id)
     {
-        return await _dbContext.Set<T>().FindAsync(id);
+        var single = await _dbContext.Set<T>().FindAsync(id);
+        return single;
     }
-
     public async Task<IReadOnlyList<T>> ListAllAsync()
     {
-        return await _dbContext.Set<T>().ToListAsync();
+        var all = await _dbContext.Set<T>().ToListAsync();
+        return all;
+    }
+
+    public async Task<T> GetEntityWithSpecification(ISpecification<T> specification)
+    {
+        return await ApplySpecification(specification).FirstOrDefaultAsync();
+    }
+    public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification)
+    {
+        return await ApplySpecification(specification).ToListAsync();
+    }
+
+    private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+    {
+        var apply = SpecificationEvaluator<T>.GetQuery(_dbContext.Set<T>().AsQueryable(), specification);
+        return apply;
     }
 }
