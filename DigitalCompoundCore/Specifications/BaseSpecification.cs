71cadf89bcfc0365eaf87de27f4aef78269efd34
A <a.kotynski@hotmail.com>
2023-04-05 08:36:28 +0200

    Added Specifications folder; added ISpecification interface; added BaseSpecification class with implemented ISpecification interface

---
+++ DigitalCompoundCore/Specifications/BaseSpecification.cs
@@ -0,0 +1,19 @@
+using System.Linq.Expressions;
+
+namespace DigitalCompoundCore.Specifications;
+
+public class BaseSpecification<T> : ISpecification<T>
+{
+    public BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+
+    public System.Linq.Expressions.Expression<Func<T, bool>> Criteria {get; }
+    public List<System.Linq.Expressions.Expression<Func<T, object>>> Includes {get; } = new List<Expression<Func<T, object>>>();
+
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+}
